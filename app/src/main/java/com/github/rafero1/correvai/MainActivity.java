12d7e2b161a8c0f973c5e262a9c342cfd3c06424
arcerix <rafael.avilar1@gmail.com>
2018-04-20 13:19:57 -0300

    Color scheme change. Weather service integration with OpenWeatherMap.

--- app/src/main/java/com/github/rafero1/correvai/MainActivity.java
+++ app/src/main/java/com/github/rafero1/correvai/MainActivity.java
@@ -2,99 +2,108 @@ package com.github.rafero1.correvai;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.media.Image;
+import android.os.Bundle;
 import android.os.SystemClock;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Chronometer;
-import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.kwabenaberko.openweathermaplib.Units;
+import com.kwabenaberko.openweathermaplib.implementation.OpenWeatherMapHelper;
+import com.kwabenaberko.openweathermaplib.models.currentweather.CurrentWeather;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+    //TODO: change to google play gps service
+    // Permission array. Checks for all of these during onCreate.
+    static final String[] PERMISSIONS = {
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.ACCESS_NETWORK_STATE
+    };
+
     LocationManager locationManager;
-    LocationListener locationListener;
     String locationProvider;
     Criteria criteria;
     Location lastKnownLocation;
+    OpenWeatherMapHelper weatherMapHelper;
 
     float mTotalDistance = 0;
     boolean mRunning = false;
     float mWalkedDistance = 0;
-    float mAvgSpeed;
+    float mAvgSpeed = 0;
 
     TextView mDistanceView;
     TextView mSpeedView;
+    TextView mWeatherView;
     Chronometer mChronometer;
     Button mButton;
-    ImageButton mShareButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}, 0);
+        // Request missing permissions on start
+        for (String permission : PERMISSIONS) {
+            List<String> requestsToMake = new ArrayList<>();
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                requestsToMake.add(permission);
+            }
+            ActivityCompat.requestPermissions(this, requestsToMake.toArray(new String[PERMISSIONS.length]), 0);
         }
 
-        mDistanceView = (TextView) findViewById(R.id.distanceView);
-        mSpeedView = (TextView) findViewById(R.id.speedView);
-        mChronometer = (Chronometer) findViewById(R.id.chronometer);
-        mButton = (Button) findViewById(R.id.button);
-        mShareButton = (ImageButton) findViewById(R.id.shareButton);
+        // Initialize View objects
+        mDistanceView = findViewById(R.id.distanceView);
+        mSpeedView = findViewById(R.id.speedView);
+        mWeatherView = findViewById(R.id.weatherView);
+        mChronometer = findViewById(R.id.chronometer);
+        mButton = findViewById(R.id.button);
 
-
+        // Setup location tracking service
         criteria = new Criteria();
         locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        if (locationManager != null)
+        if (locationManager != null) {
             locationProvider = locationManager.getBestProvider(criteria, true);
 
-        locationListener = new LocationListener() {
+            // Get last known location
+            lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+
+            // Start requesting updates with locationProvider and set listener. minTime and minDistance choose minimum time/distance between updates
+            locationManager.requestLocationUpdates(locationProvider, 0, 0, getLocationListener());
+        }
+
+        // Setup OpenWeatherMap helper class
+        weatherMapHelper = new OpenWeatherMapHelper();
+        weatherMapHelper.setApiKey(getString(R.string.OPEN_WEATHER_MAP_KEY));
+        weatherMapHelper.setUnits(Units.METRIC);
+        weatherMapHelper.getCurrentWeatherByGeoCoordinates(lastKnownLocation.getLatitude(), lastKnownLocation.getLongitude(), new OpenWeatherMapHelper.CurrentWeatherCallback() {
+
             @Override
-            public void onLocationChanged(Location location) {
-                if (mRunning) {
-                    mWalkedDistance = lastKnownLocation.distanceTo(location);
-                    mTotalDistance += mWalkedDistance;
-                    Log.d("LCT", "Distância total: " + String.valueOf(mTotalDistance));
-                    mDistanceView.setText(getShortenedTotalDistance(mTotalDistance));
-                }
+            public void onSuccess(CurrentWeather currentWeather) {
+                mWeatherView.setText(currentWeather.getMain().getTemp() + " °C");
             }
 
             @Override
-            public void onStatusChanged(String s, int i, Bundle bundle) {
-                locationProvider = locationManager.getBestProvider(criteria, true);
-                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                        && ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    return;
-                }
-                lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+            public void onFailure(Throwable throwable) {
+                mWeatherView.setText(R.string.connection_lost);
             }
 
-            @Override
-            public void onProviderEnabled(String s) {
-                //
-            }
+        });
 
-            @Override
-            public void onProviderDisabled(String s) {
-                //
-            }
-        };
-
-        lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
-        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
-
+        // View listeners
         mButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -114,42 +123,82 @@ public class MainActivity extends AppCompatActivity {
         mChronometer.setOnChronometerTickListener(new Chronometer.OnChronometerTickListener() {
             @Override
             public void onChronometerTick(Chronometer chronometer) {
-                long secondsPassed = (SystemClock.elapsedRealtime() - chronometer.getBase())/1000;
-                if(secondsPassed != 0) {
-                   float avgSpeedInMps = mWalkedDistance / secondsPassed;
+                long secondsPassed = (SystemClock.elapsedRealtime() - chronometer.getBase()) / 1000;
+                if (secondsPassed != 0) {
+                    float avgSpeedInMps = mWalkedDistance / secondsPassed;
                     mAvgSpeed = avgSpeedInMps * 3.6f;
                     mSpeedView.setText(String.format("%.2f km/h", mAvgSpeed));
                 }
             }
         });
 
-        mShareButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent sendMessage = new Intent();
-                sendMessage.setAction(Intent.ACTION_SEND);
-                sendMessage.putExtra(Intent.EXTRA_TEXT, "Acabei de correr cerca de " +getShortenedTotalDistance(mTotalDistance)+ ". Quero ver alguem fazer mais!");
-                sendMessage.setType("text/plain");
-                startActivity(Intent.createChooser(sendMessage, getResources().getText(R.string.send_to)));
-            }
-        });
-
     }
 
-    public String getShortenedTotalDistance(float d){
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+
+    public String getShortenedTotalDistance(float d) {
         String suffix = "m";
         float shortenedDistance = d;
-        if (d >= 1000){
-            shortenedDistance = d/1000;
+        if (d >= 1000) {
+            shortenedDistance = d / 1000;
             suffix = "km";
         }
         return String.format("%.2f%s", shortenedDistance, suffix);
     }
 
-    private void resetView(){
+
+    private void resetView() {
         mTotalDistance = 0;
         mAvgSpeed = 0;
         mDistanceView.setText("0m");
         mSpeedView.setText("0 km/h");
     }
+
+
+    // Listeners
+    private LocationListener getLocationListener() {
+        LocationListener l = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                if (mRunning) {
+                    mWalkedDistance = lastKnownLocation.distanceTo(location);
+                    mTotalDistance += mWalkedDistance;
+                    Log.d("LCT", "Distância total: " + String.valueOf(mTotalDistance));
+                    mDistanceView.setText(getShortenedTotalDistance(mTotalDistance));
+                }
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+                if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                        ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+
+                locationProvider = locationManager.getBestProvider(criteria, true);
+                lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+                //
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+                //
+            }
+        };
+        return l;
+    }
+
+    //TODO: private Chronometer.OnChronometerTickListener
+
+    //TODO: private Button.OnClickListener
+
 }
